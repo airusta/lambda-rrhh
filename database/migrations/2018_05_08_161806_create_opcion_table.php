47fb8d490641e561310b9a150db45cf4d9219b67
SergioTM <sergiotm1495@gmail.com>
2018-05-08 12:41:59 -0400

    varias tablas

---
+++ database/migrations/2018_05_08_161806_create_opcion_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOpcionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('opcion', function (Blueprint $table) {
+            $table->increments('id_opcion');
+            $table->integer('usuario_ini');
+            $table->integer('usuario_mod')->nullable();
+            $table->integer('usuario_del')->nullable();
+            $table->dateTime('fch_ini');
+            $table->dateTime('fch_mod')->nullable();
+            $table->dateTime('fch_del')->nullable();
+            $table->string('host_ini');
+            $table->string('host_mod',50)->nullable();
+            $table->string('host_del',50)->nullable();
+            $table->string('codigo',50);
+            $table->string('descripcion_corta',50)->nullable();
+            $table->string('descripcion_larga',50)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('opcion');
+    }
+}
