c432473f26be15c74c6c24e29f5c536f2081642e
OliverPardoM <oliverdj87@gmail.com>
2018-05-08 22:28:17 -0400

    Create all seeders

---
+++ database/seeds/PrivilegioTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PrivilegioTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
---
+++ database/seeds/RolPrivilegioTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RolPrivilegioTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
