45a66d0e78a15f0330a7a83ec7ac15495fe1bad8
alazarogutierrez <a.lazaro.g7@gmail.com>
2018-05-14 08:26:39 -0400

    correcciones auth

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\User;
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
@@ -50,7 +51,7 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
+            'email' => 'required|string|email|max:255|unique:usuario',
             'password' => 'required|string|min:6|confirmed',
         ]);
     }
@@ -64,9 +65,12 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+            'nombre' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'usuario_ini'=>'1',
+            'fch_ini'=>Carbon::now(),
+            'host_ini'=>'127.0.0.1',
         ]);
     }
 }
--- app/User.php
+++ app/User.php
@@ -9,27 +9,24 @@ class User extends Authenticatable
 {
     //use Notifiable;
     protected  $table = 'usuario';
-    protected $primary_key = 'id_usuario';
+    protected $primaryKey = 'id_usuario';
     public $timestamps = false;
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-    /*
+
     protected $fillable = [
-        'name', 'email', 'password',
+        'nombre', 'email', 'password','usuario_ini','fch_ini','host_ini',
     ];
-    */
     /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
      */
-    /*
     protected $hidden = [
         'password', 'remember_token',
     ];
-    */
 
 }
